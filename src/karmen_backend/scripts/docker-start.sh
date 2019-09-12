fd76d3b568c1dcbb0f27087ffabb5c07a88112e2
Jirka Chadima <chadima.jiri@gmail.com>
2019-09-12 11:48:18 +0200

    backend: Adds celery related services

--- src/karmen_backend/scripts/docker-start.sh
+++ src/karmen_backend/scripts/docker-start.sh
@@ -9,5 +9,14 @@ if [ "$SERVICE" = 'flask' ]; then
     export FLASK_DEBUG=true
     flask run --host=0.0.0.0 --port 8080
   fi
+elif [ "$SERVICE" = 'celery-beat' ]; then
+  export FLASK_APP=server
+  export FLASK_DEBUG=true
+  celery -A server.celery beat
+elif [ "$SERVICE" = 'celery-worker' ]; then
+  export FLASK_APP=server
+  export FLASK_DEBUG=true
+  celery -A server.celery worker
+else
+  exit 1
 fi
-
