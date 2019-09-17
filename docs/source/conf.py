7abcb15e3c30062f538b590b1a4def2ae2e8b515
Jirka Chadima <chadima.jiri@gmail.com>
2019-09-17 13:33:18 +0200

    docs: Adds logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,12 @@ exclude_patterns = []
 #
 html_theme = 'alabaster'
 
+html_theme_options = {
+    'logo': 'logo.svg',
+    'github_user': 'fragaria',
+    'github_repo': 'karmen',
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
