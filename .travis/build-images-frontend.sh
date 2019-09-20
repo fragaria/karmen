4704b8f072d6fec199d72482228e43ba0f7a5215
Jirka Chadima <chadima.jiri@gmail.com>
2019-09-20 23:16:18 +0200

    docker: Adds frontend arm/amd64 build

---
+++ .travis/build-images-frontend.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# kudos https://dev.to/zeerorg/build-multi-arch-docker-images-on-travis-5428
+
+DIR=`dirname "$0"`
+
+cd "${DIR}/../src/karmen_frontend"
+
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin
+docker info
+
+docker build -t fragaria/karmen-frontend:build -f Dockerfile.build .
+
+docker create --name extract fragaria/karmen-frontend:build
+docker cp extract:/usr/src/app/build ./build
+docker rm -f extract
+
+# Build for armhf and push
+buildctl build --frontend dockerfile.v0 \
+            --local dockerfile=. \
+            --local context=. \
+            --output type=image,name=docker.io/fragaria/karmen-frontend:$TRAVIS_BRANCH-amd64,push=true \
+            --opt platform=linux/amd64 \
+            --opt filename=./Dockerfile.serve
+
+
+buildctl build --frontend dockerfile.v0 \
+            --local dockerfile=. \
+            --local context=. \
+            --output type=image,name=docker.io/fragaria/karmen-frontend:$TRAVIS_BRANCH-armhf,push=true \
+            --opt platform=linux/armhf \
+            --opt filename=./Dockerfile.serve
+
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+# Create manifest list and push that
+docker manifest create fragaria/karmen-frontend:$TRAVIS_BRANCH \
+            fragaria/karmen-frontend:$TRAVIS_BRANCH-amd64 \
+            fragaria/karmen-frontend:$TRAVIS_BRANCH-armhf
+
+docker manifest annotate fragaria/karmen-frontend:$TRAVIS_BRANCH fragaria/karmen-frontend:$TRAVIS_BRANCH-armhf --arch arm
+docker manifest annotate fragaria/karmen-frontend:$TRAVIS_BRANCH fragaria/karmen-frontend:$TRAVIS_BRANCH-amd64 --arch amd64
+
+docker manifest push fragaria/karmen-frontend:$TRAVIS_BRANCH
+
+# latest
+
+docker pull fragaria/karmen-frontend:$TRAVIS_BRANCH
+docker pull fragaria/karmen-frontend:$TRAVIS_BRANCH-amd64
+docker pull fragaria/karmen-frontend:$TRAVIS_BRANCH-armhf
+docker tag fragaria/karmen-frontend:$TRAVIS_BRANCH fragaria/karmen-frontend:latest
+docker tag fragaria/karmen-frontend:$TRAVIS_BRANCH-amd64 fragaria/karmen-frontend:latest-amd64
+docker tag fragaria/karmen-frontend:$TRAVIS_BRANCH-armhf fragaria/karmen-frontend:latest-armhf
+docker push fragaria/karmen-frontend:latest
+docker push fragaria/karmen-frontend:latest-armhf
+docker push fragaria/karmen-frontend:latest-amd64
+
+docker manifest create fragaria/karmen-frontend:latest \
+            fragaria/karmen-frontend:latest-amd64 \
+            fragaria/karmen-frontend:latest-armhf
+
+docker manifest annotate fragaria/karmen-frontend:latest fragaria/karmen-frontend:latest-armhf --arch arm
+docker manifest annotate fragaria/karmen-frontend:latest fragaria/karmen-frontend:latest-amd64 --arch amd64
+
+docker manifest push fragaria/karmen-frontend:latest
