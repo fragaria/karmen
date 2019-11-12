f164819732892925a233b7ab3bd049baf1b6dfbf
Jirka Chadima <chadima.jiri@gmail.com>
2019-11-12 19:56:26 +0000

    backend: Adds endpoints for refresh and fresh access token

--- src/karmen_backend/server/routes/users.py
+++ src/karmen_backend/server/routes/users.py
@@ -1,7 +1,13 @@
 import bcrypt
-from flask import jsonify, request, abort, send_file
+from flask import jsonify, request, abort
 from flask_cors import cross_origin
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    jwt_refresh_token_required,
+    get_jwt_identity,
+)
 from server import app, jwt
 from server.database import users, local_users
 
@@ -19,10 +25,7 @@ def user_identity_lookup(user):
     return user["uuid"]
 
 
-# This is intentionally not called login as we might use that for the OAuth process in the future
-@app.route("/users/authenticate", methods=["POST", "OPTIONS"])
-@cross_origin()
-def authenticate():
+def authenticate_base(include_refresh_token):
     data = request.json
     if not data:
         return abort(400)
@@ -44,6 +47,81 @@ def authenticate():
 
     userdata = dict(user)
     userdata.update(local)
-    access_token = create_access_token(identity=userdata, fresh=True)
-    refresh_token = create_refresh_token(identity=userdata)
-    return jsonify(access_token=access_token, refresh_token=refresh_token), 200
+    response = {"access_token": create_access_token(identity=userdata, fresh=True)}
+    if include_refresh_token:
+        response["refresh_token"] = create_refresh_token(identity=userdata)
+    return jsonify(response), 200
+
+
+# This is intentionally not called login as we might use that for the OAuth process in the future
+# This returns a fresh access token and a refresh token
+@app.route("/users/authenticate", methods=["POST", "OPTIONS"])
+@cross_origin()
+def authenticate():
+    return authenticate_base(True)
+
+
+# This returns a fresh access token and no refresh token
+@app.route("/users/authenticate-fresh", methods=["POST", "OPTIONS"])
+@cross_origin()
+def authenticate_fresh():
+    return authenticate_base(False)
+
+
+# This returns a non fresh access token and no refresh token
+@app.route("/users/authenticate-refresh", methods=["POST", "OPTIONS"])
+@jwt_refresh_token_required
+def refresh():
+    uuid = get_jwt_identity()
+    user = users.get_by_uuid(uuid)
+    if not user:
+        return abort(401)
+
+    local = local_users.get_local_user(user["uuid"])
+    if not local:
+        return abort(401)
+    userdata = dict(user)
+    userdata.update(local)
+    return jsonify({"access_token": create_access_token(identity=userdata)}), 200
+
+
+@app.route("/users/<uuid>", methods=["PATCH", "OPTIONS"])
+@cross_origin()
+@jwt_required
+def change_password(uuid):
+    # TODO restrict this only for fresh access_tokens
+    data = request.json
+    if not data:
+        return abort(400)
+    password = data.get("password", None)
+    new_password = data.get("new_password", None)
+    new_password_confirmation = data.get("new_password_confirmation", None)
+    if (
+        not password
+        or not new_password
+        or not new_password_confirmation
+        or new_password != new_password_confirmation
+    ):
+        return abort(400)
+
+    if get_jwt_identity() != uuid:
+        return abort(401)
+
+    # TODO check for disabled users
+    user = users.get_by_uuid(uuid)
+    if not user:
+        return abort(401)
+
+    local = local_users.get_local_user(user["uuid"])
+    if not local:
+        return abort(401)
+
+    if not bcrypt.checkpw(password.encode("utf8"), local["pwd_hash"].encode("utf8")):
+        return abort(401)
+
+    pwd_hash = bcrypt.hashpw(new_password.encode("utf8"), bcrypt.gensalt())
+    local_users.update_local_user(
+        pwd_hash=pwd_hash.decode("utf8"), force_pwd_change=False, uuid=uuid
+    )
+
+    return "", 200
--- src/karmen_backend/tests/routes/test_users.py
+++ src/karmen_backend/tests/routes/test_users.py
@@ -52,6 +52,7 @@ class AuthenticateRoute(unittest.TestCase):
             )
             self.assertEqual(response.status_code, 200)
             self.assertTrue("access_token" in response.json)
+            self.assertTrue("refresh_token" in response.json)
             data = get_token_data(response.json["access_token"])
             self.assertEqual(data["fresh"], True)
             self.assertEqual(data["type"], "access")
@@ -60,15 +61,246 @@ class AuthenticateRoute(unittest.TestCase):
             self.assertTrue("role" in data["user_claims"])
             self.assertTrue("force_pwd_change" in data["user_claims"])
 
-    def test_returns_refresh_token(self):
+
+class AuthenticateFreshRoute(unittest.TestCase):
+    def test_no_data(self):
+        with app.test_client() as c:
+            response = c.post("/users/authenticate-fresh")
+            self.assertEqual(response.status_code, 400)
+
+    def test_missing_username(self):
+        with app.test_client() as c:
+            response = c.post("/users/authenticate-fresh", json={"password": "random"})
+            self.assertEqual(response.status_code, 400)
+
+    def test_missing_password(self):
+        with app.test_client() as c:
+            response = c.post("/users/authenticate-fresh", json={"username": "random"})
+            self.assertEqual(response.status_code, 400)
+
+    def test_unknown_user(self):
+        with app.test_client() as c:
+            response = c.post(
+                "/users/authenticate-fresh",
+                json={"username": "random", "password": "random"},
+            )
+            self.assertEqual(response.status_code, 401)
+
+    def test_bad_password(self):
+        with app.test_client() as c:
+            response = c.post(
+                "/users/authenticate-fresh",
+                json={"username": "test-admin", "password": "random"},
+            )
+            self.assertEqual(response.status_code, 401)
+
+    def test_returns_fresh_access_token(self):
+        with app.test_client() as c:
+            response = c.post(
+                "/users/authenticate-fresh",
+                json={"username": "test-admin", "password": "admin-password"},
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue("access_token" in response.json)
+            self.assertTrue("refresh_token" not in response.json)
+            data = get_token_data(response.json["access_token"])
+            self.assertEqual(data["fresh"], True)
+            self.assertEqual(data["type"], "access")
+            self.assertEqual(data["identity"], "6480fa7d-ce18-4ae2-818b-f1d200050806")
+            self.assertTrue("user_claims" in data)
+            self.assertTrue("role" in data["user_claims"])
+            self.assertTrue("force_pwd_change" in data["user_claims"])
+
+
+class AuthenticateRefreshRoute(unittest.TestCase):
+    def setUp(self):
         with app.test_client() as c:
             response = c.post(
                 "/users/authenticate",
                 json={"username": "test-admin", "password": "admin-password"},
             )
+            self.jwt = response.json["refresh_token"]
+
+    def test_no_token(self):
+        with app.test_client() as c:
+            response = c.post("/users/authenticate-refresh")
+            self.assertEqual(response.status_code, 401)
+
+    def test_bad_token(self):
+        with app.test_client() as c:
+            response = c.post(
+                "/users/authenticate-refresh",
+                headers={
+                    "Authorization": "Bearer %s"
+                    % (
+                        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ0b3B0YWwuY29tIiwiZXhwIjoxNDI2NDIwODAwLCJodHRwOi8vdG9wdGFsLmNvbS9qd3RfY2xhaW1zL2lzX2FkbWluIjp0cnVlLCJjb21wYW55IjoiVG9wdGFsIiwiYXdlc29tZSI6dHJ1ZX0.yRQYnWzskCZUxPwaQupWkiUzKELZ49eM7oWxAQK_ZXw",
+                    )
+                },
+            )
+            self.assertEqual(response.status_code, 422)
+
+    def test_returns_nonfresh_access_token(self):
+        with app.test_client() as c:
+            response = c.post(
+                "/users/authenticate-refresh",
+                headers={"Authorization": "Bearer %s" % (self.jwt,)},
+            )
             self.assertEqual(response.status_code, 200)
-            self.assertTrue("refresh_token" in response.json)
-            data = get_token_data(response.json["refresh_token"])
-            self.assertEqual(data["type"], "refresh")
-            self.assertTrue("fresh" not in data)
+            self.assertTrue("access_token" in response.json)
+            self.assertTrue("refresh_token" not in response.json)
+            data = get_token_data(response.json["access_token"])
+            self.assertEqual(data["fresh"], False)
+            self.assertEqual(data["type"], "access")
             self.assertEqual(data["identity"], "6480fa7d-ce18-4ae2-818b-f1d200050806")
+            self.assertTrue("user_claims" in data)
+            self.assertTrue("role" in data["user_claims"])
+            self.assertTrue("force_pwd_change" in data["user_claims"])
+
+
+class ChangePasswordRoute(unittest.TestCase):
+    def setUp(self):
+        with app.test_client() as c:
+            response = c.post(
+                "/users/authenticate",
+                json={"username": "test-admin", "password": "admin-password"},
+            )
+            self.jwt = response.json["access_token"]
+
+    def test_missing_jwt(self):
+        with app.test_client() as c:
+            response = c.patch(
+                "/users/6480fa7d-ce18-4ae2-818b-f1d200050806",
+                json={
+                    "password": "random",
+                    "new_password_confirmation": "random",
+                    "new_password": "random",
+                },
+            )
+            self.assertEqual(response.status_code, 401)
+
+    def test_mismatch_token_uuid(self):
+        with app.test_client() as c:
+            response = c.patch(
+                "/users/77315957-8ebb-4a44-976c-758dbf28bb9f",
+                headers={"Authorization": "Bearer %s" % (self.jwt,)},
+                json={
+                    "password": "user-password",
+                    "new_password_confirmation": "random",
+                    "new_password": "random",
+                },
+            )
+            self.assertEqual(response.status_code, 401)
+
+    def test_no_data(self):
+        with app.test_client() as c:
+            response = c.patch(
+                "/users/6480fa7d-ce18-4ae2-818b-f1d200050806",
+                headers={"Authorization": "Bearer %s" % (self.jwt,)},
+            )
+            self.assertEqual(response.status_code, 400)
+
+    def test_missing_new_password(self):
+        with app.test_client() as c:
+            response = c.patch(
+                "/users/6480fa7d-ce18-4ae2-818b-f1d200050806",
+                headers={"Authorization": "Bearer %s" % (self.jwt,)},
+                json={"password": "random", "new_password_confirmation": "random"},
+            )
+            self.assertEqual(response.status_code, 400)
+
+    def test_missing_new_password_confirmation(self):
+        with app.test_client() as c:
+            response = c.patch(
+                "/users/6480fa7d-ce18-4ae2-818b-f1d200050806",
+                headers={"Authorization": "Bearer %s" % (self.jwt,)},
+                json={"password": "random", "new_password": "random"},
+            )
+            self.assertEqual(response.status_code, 400)
+
+    def test_missing_password(self):
+        with app.test_client() as c:
+            response = c.patch(
+                "/users/6480fa7d-ce18-4ae2-818b-f1d200050806",
+                headers={"Authorization": "Bearer %s" % (self.jwt,)},
+                json={"new_password_confirmation": "random", "new_password": "random"},
+            )
+            self.assertEqual(response.status_code, 400)
+
+    def test_unknown_user(self):
+        with app.test_client() as c:
+            response = c.patch(
+                "/users/6480fa7d-ce18-4ae2-1234-f1d200050806",
+                headers={"Authorization": "Bearer %s" % (self.jwt,)},
+                json={
+                    "new_password_confirmation": "random",
+                    "new_password": "random",
+                    "password": "admin-password",
+                },
+            )
+            self.assertEqual(response.status_code, 401)
+
+    def test_bad_password(self):
+        with app.test_client() as c:
+            response = c.patch(
+                "/users/6480fa7d-ce18-4ae2-818b-f1d200050806",
+                json={
+                    "new_password_confirmation": "random",
+                    "new_password": "random",
+                    "password": "bad-password",
+                },
+            )
+            self.assertEqual(response.status_code, 401)
+
+    def test_new_pwd_mismatch(self):
+        with app.test_client() as c:
+            response = c.patch(
+                "/users/6480fa7d-ce18-4ae2-818b-f1d200050806",
+                headers={"Authorization": "Bearer %s" % (self.jwt,)},
+                json={
+                    "new_password_confirmation": "random",
+                    "new_password": "random-mismatch",
+                    "password": "admin-password",
+                },
+            )
+            self.assertEqual(response.status_code, 400)
+
+    def test_bad_pwd(self):
+        with app.test_client() as c:
+            response = c.patch(
+                "/users/6480fa7d-ce18-4ae2-818b-f1d200050806",
+                headers={"Authorization": "Bearer %s" % (self.jwt,)},
+                json={
+                    "new_password_confirmation": "random",
+                    "new_password": "random",
+                    "password": "bad-password",
+                },
+            )
+            self.assertEqual(response.status_code, 401)
+
+    def test_pwd_changed(self):
+        with app.test_client() as c:
+            change = c.patch(
+                "/users/6480fa7d-ce18-4ae2-818b-f1d200050806",
+                headers={"Authorization": "Bearer %s" % (self.jwt,)},
+                json={
+                    "new_password_confirmation": "random",
+                    "new_password": "random",
+                    "password": "admin-password",
+                },
+            )
+            self.assertEqual(change.status_code, 200)
+            auth = c.post(
+                "/users/authenticate",
+                json={"username": "test-admin", "password": "random"},
+            )
+            self.assertEqual(auth.status_code, 200)
+            change_back = c.patch(
+                "/users/6480fa7d-ce18-4ae2-818b-f1d200050806",
+                headers={"Authorization": "Bearer %s" % (auth.json["access_token"],)},
+                json={
+                    "new_password_confirmation": "admin-password",
+                    "new_password": "admin-password",
+                    "password": "random",
+                },
+            )
+            self.assertEqual(change_back.status_code, 200)
