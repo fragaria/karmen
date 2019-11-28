f4509c71882ad15b5f5c3c36d96044b99533622d
Jirka Chadima <chadima.jiri@gmail.com>
2019-11-28 16:25:56 +0100

    Version 0.0.15

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, <a href="https://fragaria.cz">Fragaria s.r.o.</a>'
 author = 'Fragaria s.r.o.'
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.14"
+release = "0.0.15"
 
 
 # -- General configuration ---------------------------------------------------
