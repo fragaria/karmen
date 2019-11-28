26e265f8238c410d80263b282460c13238f03660
Jirka Chadima <chadima.jiri@gmail.com>
2019-11-28 17:51:39 +0100

    frontend: Supports IE 11

--- src/karmen_frontend/src/index.js
+++ src/karmen_frontend/src/index.js
@@ -1,3 +1,9 @@
+import 'react-app-polyfill/ie11';
+import 'core-js/es/promise';
+import 'core-js/es/object';
+import 'core-js/es/array';
+import 'core-js/proposals/url';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
