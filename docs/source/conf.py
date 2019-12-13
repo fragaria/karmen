81b3d3ef037228e3c875b041e4b169128366e1b1
Jirka Chadima <chadima.jiri@gmail.com>
2019-12-13 15:05:22 +0100

    Version 0.2.0-rc1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, <a href="https://fragaria.cz">Fragaria s.r.o.</a>'
 author = 'Fragaria s.r.o.'
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.2.0-rc1"
 
 
 # -- General configuration ---------------------------------------------------
