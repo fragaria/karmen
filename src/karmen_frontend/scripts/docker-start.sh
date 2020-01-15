bc325d47b7384e65f95d84d9f440a89a466f887e
Jirka Chadima <chadima.jiri@gmail.com>
2020-01-15 21:00:30 +0100

    docker: Decreases size of frontend and proxy images

--- src/karmen_frontend/scripts/docker-start.sh
+++ src/karmen_frontend/scripts/docker-start.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+
+DIR=$(dirname $(realpath -s $0))
+cd $DIR
+
 if [ "$ENV" = 'production' ]; then
-  npm run build-env-file
+  if [ -n ${BACKEND_BASE} ]; then
+    cat << EOF > "../build/env.js"
+window.env = {
+  BACKEND_BASE: "${BACKEND_BASE}"
+};
+EOF
+  fi
+
   SERVICE_HOST=${SERVICE_HOST:-0.0.0.0}
   SERVICE_PORT=${SERVICE_PORT:-9765}
   envsubst '$SERVICE_PORT $SERVICE_HOST' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
