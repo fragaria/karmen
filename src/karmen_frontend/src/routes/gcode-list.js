19f583a0a5b55c014bb3129e4663cdf52009b65c
Tibor Kulcar <kulcar.tibor@gmail.com>
2020-01-16 14:56:03 +0100

    set pagination start to 15 items

--- src/karmen_frontend/src/routes/gcode-list.js
+++ src/karmen_frontend/src/routes/gcode-list.js
@@ -297,7 +297,7 @@ class GcodeList extends React.Component {
       ];
       page = 0;
     }
-    getGcodes(pages[page].startWith, newOrderBy, newFilter, 3, [
+    getGcodes(pages[page].startWith, newOrderBy, newFilter, 15, [
       "id",
       "display",
       "filename",
