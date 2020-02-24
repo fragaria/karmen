c4c549b9c825d4457ad3b1b3f2b163188299e3df
Jirka Chadima <chadima.jiri@gmail.com>
2020-02-24 14:46:35 +0100

    Version 0.7.0-rc6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, <a href="https://fragaria.cz">Fragaria s.r.o.</a>'
 author = 'Fragaria s.r.o.'
 
 # The full version, including alpha/beta/rc tags
-release = "0.7.0-rc5"
+release = "0.7.0-rc6"
 
 
 # -- General configuration ---------------------------------------------------
