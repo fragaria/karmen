15907ef6ffe8d68e8e006038ead2a55013cef94a
Jirka Chadima <chadima.jiri@gmail.com>
2020-03-18 15:15:19 +0100

    frontend: Fixes linter issue

--- src/karmen_frontend/src/services/backend/utils.js
+++ src/karmen_frontend/src/services/backend/utils.js
@@ -113,11 +113,14 @@ export const performRequest = opts => {
         console.error(`Request ${opts.uri} failed: ${response.status}`);
       }
       if (opts.parseResponse) {
-        return response.json().then(data => {
-          return { status: response.status, ...opts.appendData, data };
-        }).catch((e) => {
-          return { status: response.status, ...opts.appendData };
-        })
+        return response
+          .json()
+          .then(data => {
+            return { status: response.status, ...opts.appendData, data };
+          })
+          .catch(e => {
+            return { status: response.status, ...opts.appendData };
+          });
       }
       return { status: response.status, ...opts.appendData };
     })
