addfc777ceb2be974a1cd2979c4caf28723a1590
Jirka Chadima <chadima.jiri@gmail.com>
2020-03-24 14:49:11 +0100

    Version 0.9.0-rc6

--- src/karmen_backend/server/__init__.py
+++ src/karmen_backend/server/__init__.py
@@ -8,7 +8,7 @@ from flask_jwt_extended import JWTManager
 from flask_executor import Executor
 from sentry_sdk.integrations.flask import FlaskIntegration
 
-__version__ = "0.9.0-rc5"
+__version__ = "0.9.0-rc6"
 __author__ = "Jirka Chadima"
 __copyright__ = (
     "Copyright (C) 2019 Fragaria s.r.o. - Released under terms of AGPLv3 License"
