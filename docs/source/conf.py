7f977904c0480b648c999d84589b220be361935c
semik <martin.bilek@gmail.com>
2020-04-29 14:02:44 +0200

    Version 0.9.0-rc15

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-__version__ = "v0.9.0-rc12"
+__version__ = "0.9.0-rc15"
 
 
 # -- Project information -----------------------------------------------------
