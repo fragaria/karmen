03ca96da31f9ae77ed44f58a02e98b693da952d1
Adam Ježek <mail@ajezek.cz>
2020-05-22 15:45:19 +0200

    Version 0.11.0-rc3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-__version__ = "0.11.0-rc2"
+__version__ = "0.11.0-rc3"
 
 # -- Project information -----------------------------------------------------
 
