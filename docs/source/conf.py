c97b3edbf3fc0aaa818d2dddc8ba5bc261a6e3ca
Adam Ježek <mail@ajezek.cz>
2020-05-22 20:33:37 +0200

    Version 0.11.0-ts5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-__version__ = "0.11.0-ts4"
+__version__ = "0.11.0-ts5"
 
 # -- Project information -----------------------------------------------------
 
