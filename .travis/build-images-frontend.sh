ed21515fe67e4b2faa7dbfd109b4a9c0f4f3fc0c
Adam Ježek <mail@ajezek.cz>
2020-05-22 18:10:38 +0200

    changed build to buildx

--- .travis/build-images-frontend.sh
+++ .travis/build-images-frontend.sh
@@ -4,8 +4,12 @@ set -e
 # kudos https://dev.to/zeerorg/build-multi-arch-docker-images-on-travis-5428
 
 DIR=$(dirname $(realpath -s $0))
+DOCKER_REPO=fragaria/karmen_frontend
 
 cd "${DIR}/../src/karmen_frontend"
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin
 docker info
@@ -16,57 +20,22 @@ docker create --name extract fragaria/karmen-frontend:build
 docker cp extract:/usr/src/app/build ./build
 docker rm -f extract
 
-# Build for amd64 and push
-buildctl build --frontend dockerfile.v0 \
-            --local dockerfile=. \
-            --local context=. \
-            --output type=image,name=docker.io/fragaria/karmen-frontend:$TRAVIS_BRANCH-amd64,push=true \
-            --opt platform=linux/amd64 \
-            --opt filename=./Dockerfile.serve
+DOCKER_PUBLISH_TAG="fragaria/karmen-frontend:$TRAVIS_BRANCH"
 
-
-# Build for armhf and push
-buildctl build --frontend dockerfile.v0 \
-            --local dockerfile=. \
-            --local context=. \
-            --output type=image,name=docker.io/fragaria/karmen-frontend:$TRAVIS_BRANCH-armhf,push=true \
-            --opt platform=linux/armhf \
-            --opt filename=./Dockerfile.serve
-
-
-export DOCKER_CLI_EXPERIMENTAL=enabled
-
-# Create manifest list and push that
-docker manifest create fragaria/karmen-frontend:$TRAVIS_BRANCH \
-            fragaria/karmen-frontend:$TRAVIS_BRANCH-amd64 \
-            fragaria/karmen-frontend:$TRAVIS_BRANCH-armhf
-
-docker manifest annotate fragaria/karmen-frontend:$TRAVIS_BRANCH fragaria/karmen-frontend:$TRAVIS_BRANCH-armhf --arch arm
-docker manifest annotate fragaria/karmen-frontend:$TRAVIS_BRANCH fragaria/karmen-frontend:$TRAVIS_BRANCH-amd64 --arch amd64
-
-docker manifest push fragaria/karmen-frontend:$TRAVIS_BRANCH
-
-# latest only if not rc
+# latest only if this is not a pre release
 if [[ "$TRAVIS_TAG" =~ ^v[0-9.]*$ ]]; then
-  docker pull fragaria/karmen-frontend:$TRAVIS_BRANCH
-  docker pull fragaria/karmen-frontend:$TRAVIS_BRANCH-amd64
-  docker pull fragaria/karmen-frontend:$TRAVIS_BRANCH-armhf
-  docker tag fragaria/karmen-frontend:$TRAVIS_BRANCH fragaria/karmen-frontend:latest
-  docker tag fragaria/karmen-frontend:$TRAVIS_BRANCH-amd64 fragaria/karmen-frontend:latest-amd64
-  docker tag fragaria/karmen-frontend:$TRAVIS_BRANCH-armhf fragaria/karmen-frontend:latest-armhf
-  docker push fragaria/karmen-frontend:latest
-  docker push fragaria/karmen-frontend:latest-armhf
-  docker push fragaria/karmen-frontend:latest-amd64
 
-  docker manifest create fragaria/karmen-frontend:latest \
-              fragaria/karmen-frontend:latest-amd64 \
-              fragaria/karmen-frontend:latest-armhf
+DOCKER_PUBLISH_TAG="fragaria/karmen-frontend:lastest"
 
-  docker manifest annotate fragaria/karmen-frontend:latest fragaria/karmen-frontend:latest-armhf --arch arm
-  docker manifest annotate fragaria/karmen-frontend:latest fragaria/karmen-frontend:latest-amd64 --arch amd64
-
-  docker manifest push fragaria/karmen-frontend:latest
 fi
+
+# Build for amd64 and push
+docker buildx build --platform=linux/amd64,linux/arm/v7 \
+            --push \
+            --tag "$DOCKER_PUBLISH_TAG" \
+            .
+
+
 
 # Delete unnecessary tags
 DOCKER_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_USER}'", "password": "'${DOCKER_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
--- .travis/build-images-proxy.sh
+++ .travis/build-images-proxy.sh
@@ -7,60 +7,28 @@ DIR=$(dirname $(realpath -s $0))
 
 cd "${DIR}/../src/proxy"
 
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin
 docker info
 
-# Build for amd64 and push
-buildctl build --frontend dockerfile.v0 \
-            --local dockerfile=. \
-            --local context=. \
-            --output type=image,name=docker.io/fragaria/karmen-proxy:$TRAVIS_BRANCH-amd64,push=true \
-            --opt platform=linux/amd64 \
-            --opt filename=./Dockerfile
 
+DOCKER_PUBLISH_TAG="fragaria/karmen-proxy:$TRAVIS_BRANCH"
 
-# Build for armhf and push
-buildctl build --frontend dockerfile.v0 \
-            --local dockerfile=. \
-            --local context=. \
-            --output type=image,name=docker.io/fragaria/karmen-proxy:$TRAVIS_BRANCH-armhf,push=true \
-            --opt platform=linux/armhf \
-            --opt filename=./Dockerfile
-
-
-export DOCKER_CLI_EXPERIMENTAL=enabled
-
-# Create manifest list and push that
-docker manifest create fragaria/karmen-proxy:$TRAVIS_BRANCH \
-            fragaria/karmen-proxy:$TRAVIS_BRANCH-amd64 \
-            fragaria/karmen-proxy:$TRAVIS_BRANCH-armhf
-
-docker manifest annotate fragaria/karmen-proxy:$TRAVIS_BRANCH fragaria/karmen-proxy:$TRAVIS_BRANCH-armhf --arch arm
-docker manifest annotate fragaria/karmen-proxy:$TRAVIS_BRANCH fragaria/karmen-proxy:$TRAVIS_BRANCH-amd64 --arch amd64
-
-docker manifest push fragaria/karmen-proxy:$TRAVIS_BRANCH
-
-# latest only if not rc
+# latest only if this is not a pre release
 if [[ "$TRAVIS_TAG" =~ ^v[0-9.]*$ ]]; then
-  docker pull fragaria/karmen-proxy:$TRAVIS_BRANCH
-  docker pull fragaria/karmen-proxy:$TRAVIS_BRANCH-amd64
-  docker pull fragaria/karmen-proxy:$TRAVIS_BRANCH-armhf
-  docker tag fragaria/karmen-proxy:$TRAVIS_BRANCH fragaria/karmen-proxy:latest
-  docker tag fragaria/karmen-proxy:$TRAVIS_BRANCH-amd64 fragaria/karmen-proxy:latest-amd64
-  docker tag fragaria/karmen-proxy:$TRAVIS_BRANCH-armhf fragaria/karmen-proxy:latest-armhf
-  docker push fragaria/karmen-proxy:latest
-  docker push fragaria/karmen-proxy:latest-armhf
-  docker push fragaria/karmen-proxy:latest-amd64
 
-  docker manifest create fragaria/karmen-proxy:latest \
-              fragaria/karmen-proxy:latest-amd64 \
-              fragaria/karmen-proxy:latest-armhf
+DOCKER_PUBLISH_TAG="fragaria/karmen-proxy:lastest"
 
-  docker manifest annotate fragaria/karmen-proxy:latest fragaria/karmen-proxy:latest-armhf --arch arm
-  docker manifest annotate fragaria/karmen-proxy:latest fragaria/karmen-proxy:latest-amd64 --arch amd64
-
-  docker manifest push fragaria/karmen-proxy:latest
 fi
+
+# Build for amd64 and push
+docker buildx build --platform=linux/amd64,linux/arm/v7 \
+            --push \
+            --tag "$DOCKER_PUBLISH_TAG" \
+            .
+
 
 # Delete unnecessary tags
 DOCKER_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_USER}'", "password": "'${DOCKER_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
