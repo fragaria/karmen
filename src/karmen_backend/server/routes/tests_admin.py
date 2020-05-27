038fb998216a18cea36c7fd154048528b8fc4f1d
Adam Ježek <mail@ajezek.cz>
2020-05-27 14:48:00 +0200

    created admin API for users managemetn during tests

--- src/karmen_backend/server/routes/tests_admin.py
+++ src/karmen_backend/server/routes/tests_admin.py
@@ -1,6 +1,58 @@
+import server.models as models
+from server import app
+import functools
+from flask import jsonify, request, abort, make_response
+import json
+from server.database import organization_roles, users, organizations
+import uuid as guid
+
+
+def local_tests_mode_required(func):
+    @functools.wraps(func)
+    def wrap(*args, **kwargs):
+        local_tests_token = request.json.get("local-tests-token")
+        if not app.config.get(
+            "LOCAL_TESTS_TOKEN"
+        ) or local_tests_token != app.config.get("LOCAL_TESTS_TOKEN"):
+            return abort(make_response(jsonify(message="Unauthorized"), 403))
+        return func(*args, **kwargs)
+
+    return wrap
+
+
+# /tests-admin/users/create, POST
+@local_tests_mode_required
 def create_test_user():
-    pass
+    email = request.json.get("email")
+    password = request.json.get("password")
+    new_user = models.users_me.create_tests_user(email=email, password=password)
+    return make_response(json.dumps(new_user), 201 if new_user["activated"] else 400)
 
 
-def add_user_to_org():
-    pass
+# /tests-admin/organizations, POST
+@local_tests_mode_required
+def create_organization():
+    name = request.json.get("name")
+    uuid = guid.uuid4()
+    name = name.lstrip().rstrip()
+    organizations.add_organization(uuid=uuid, name=name)
+    return make_response(jsonify(name=name, uuid=uuid), 201)
+
+
+#  /tests-admin/organizations/{org_uuid}/users, post
+@local_tests_mode_required
+def add_user_to_org(org_uuid):
+    user_uuid = request.json.get("uuid")
+    org_role = request.json.get("role")
+
+    user = users.get_by_uuid(user_uuid)
+    if not user:
+        return make_response("User does not exist", 400)
+    if not organizations.get_by_uuid(org_uuid):
+        return make_response("Organization does not exist", 400)
+
+    if organization_roles.get_organization_role(org_uuid, user_uuid) is not None:
+        return make_response("User is already in organization", 400)
+
+    organization_roles.set_organization_role(org_uuid, user_uuid, org_role)
+    return make_response("", 200)
