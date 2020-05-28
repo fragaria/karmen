5862abbfa22c35848339a06599293bdd885e4308
Adam Ježek <mail@ajezek.cz>
2020-05-28 12:10:38 +0200

    different proxy for dev and prod

--- src/proxy/docker-start.sh
+++ src/proxy/docker-start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 SERVICE_HOST=${SERVICE_HOST:-0.0.0.0}
 SERVICE_PORT=${SERVICE_PORT:-9766}
-envsubst '$SERVICE_HOST $SERVICE_PORT $BACKEND_HOST $BACKEND_PORT $FRONTEND_HOST $FRONTEND_PORT $APIDOC_HOST $APIDOC_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+IS_DEV_ENV=${IS_DEV_ENV}
+sh /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template.parsed
+envsubst '$SERVICE_HOST $SERVICE_PORT $BACKEND_HOST $BACKEND_PORT $FRONTEND_HOST $FRONTEND_PORT $APIDOC_HOST $APIDOC_PORT' < /etc/nginx/nginx.conf.template.parsed > /etc/nginx/nginx.conf
 nginx -g "daemon off;"
