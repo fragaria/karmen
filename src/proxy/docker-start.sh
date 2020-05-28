5cb3f5cc7db93f9d1e15789f1ae7028d1590b6d2
Adam Ježek <mail@ajezek.cz>
2020-05-28 13:54:00 +0200

    moved dev configs to separate files

--- src/proxy/docker-start.sh
+++ src/proxy/docker-start.sh
@@ -2,6 +2,17 @@
 SERVICE_HOST=${SERVICE_HOST:-0.0.0.0}
 SERVICE_PORT=${SERVICE_PORT:-9766}
 IS_DEV_ENV=${IS_DEV_ENV}
-sh /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template.parsed
-envsubst '$SERVICE_HOST $SERVICE_PORT $BACKEND_HOST $BACKEND_PORT $FRONTEND_HOST $FRONTEND_PORT $APIDOC_HOST $APIDOC_PORT' < /etc/nginx/nginx.conf.template.parsed > /etc/nginx/nginx.conf
+
+mkdir -p /etc/nginx/includes
+
+if [ "$IS_DEV_ENV" = 1 ]
+
+then
+  for configFile in /etc/nginx/includes-dev/*.conf; do
+    newFile="/etc/nginx/includes/$(basename "$configFile")"
+    envsubst '$SERVICE_HOST $SERVICE_PORT $BACKEND_HOST $BACKEND_PORT $FRONTEND_HOST $FRONTEND_PORT $APIDOC_HOST $APIDOC_PORT' < "$configFile" > "$newFile"
+  done
+fi
+
+envsubst '$SERVICE_HOST $SERVICE_PORT $BACKEND_HOST $BACKEND_PORT $FRONTEND_HOST $FRONTEND_PORT $APIDOC_HOST $APIDOC_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 nginx -g "daemon off;"
