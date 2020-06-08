44545f5f1d463b929b94c54deaa23a0e564b1e33
Adam Ježek <mail@ajezek.cz>
2020-06-08 13:28:42 +0200

    buildx test

--- .travis/build-images-backend.sh
+++ .travis/build-images-backend.sh
@@ -10,56 +10,22 @@ cd "${DIR}/../src/karmen_backend"
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin
 docker info
 
-# Build for amd64 and push
-buildctl build --frontend dockerfile.v0 \
-            --local dockerfile=. \
-            --local context=. \
-            --output type=image,name=docker.io/fragaria/karmen-backend:$TRAVIS_BRANCH-amd64,push=true \
-            --opt platform=linux/amd64 \
-            --opt filename=./Dockerfile
-
-
-# Build for armhf and push
-buildctl build --frontend dockerfile.v0 \
-            --local dockerfile=. \
-            --local context=. \
-            --output type=image,name=docker.io/fragaria/karmen-backend:$TRAVIS_BRANCH-armhf,push=true \
-            --opt platform=linux/armhf \
-            --opt filename=./Dockerfile
-
-
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-# Create manifest list and push that
-docker manifest create fragaria/karmen-backend:$TRAVIS_BRANCH \
-            fragaria/karmen-backend:$TRAVIS_BRANCH-amd64 \
-            fragaria/karmen-backend:$TRAVIS_BRANCH-armhf
+DOCKER_PUBLISH_TAG="fragaria/karmen-backend:$TRAVIS_BRANCH"
 
-docker manifest annotate fragaria/karmen-backend:$TRAVIS_BRANCH fragaria/karmen-backend:$TRAVIS_BRANCH-armhf --arch arm
-docker manifest annotate fragaria/karmen-backend:$TRAVIS_BRANCH fragaria/karmen-backend:$TRAVIS_BRANCH-amd64 --arch amd64
-
-docker manifest push fragaria/karmen-backend:$TRAVIS_BRANCH
-
-# latest only if not rc
+# latest only if this is not a pre release
 if [[ "$TRAVIS_TAG" =~ ^v[0-9.]*$ ]]; then
-  docker pull fragaria/karmen-backend:$TRAVIS_BRANCH
-  docker pull fragaria/karmen-backend:$TRAVIS_BRANCH-amd64
-  docker pull fragaria/karmen-backend:$TRAVIS_BRANCH-armhf
-  docker tag fragaria/karmen-backend:$TRAVIS_BRANCH fragaria/karmen-backend:latest
-  docker tag fragaria/karmen-backend:$TRAVIS_BRANCH-amd64 fragaria/karmen-backend:latest-amd64
-  docker tag fragaria/karmen-backend:$TRAVIS_BRANCH-armhf fragaria/karmen-backend:latest-armhf
-  docker push fragaria/karmen-backend:latest
-  docker push fragaria/karmen-backend:latest-armhf
-  docker push fragaria/karmen-backend:latest-amd64
 
-  docker manifest create fragaria/karmen-backend:latest \
-              fragaria/karmen-backend:latest-amd64 \
-              fragaria/karmen-backend:latest-armhf
-  docker manifest annotate fragaria/karmen-backend:latest fragaria/karmen-backend:latest-armhf --arch arm
-  docker manifest annotate fragaria/karmen-backend:latest fragaria/karmen-backend:latest-amd64 --arch amd64
+DOCKER_PUBLISH_TAG="fragaria/karmen-backend:lastest"
 
-  docker manifest push fragaria/karmen-backend:latest
 fi
+
+# Build for amd64 and push
+docker buildx build --platform=linux/amd64,linux/arm/v7 \
+            --push \
+            --tag "$DOCKER_PUBLISH_TAG" \
+            .
 
 # Delete unnecessary tags
 DOCKER_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_USER}'", "password": "'${DOCKER_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
