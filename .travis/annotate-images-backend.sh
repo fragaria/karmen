580e11000f844a3f245d862eb3b0c3456484c994
Adam Ježek <mail@ajezek.cz>
2020-06-09 16:03:23 +0200

    travis test

--- .travis/annotate-images-backend.sh
+++ .travis/annotate-images-backend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -v
 
 # kudos https://dev.to/zeerorg/build-multi-arch-docker-images-on-travis-5428
 
@@ -14,15 +15,28 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 
 
 # Create manifest list and push that
-docker manifest create fragaria/karmen-backend:$TRAVIS_BRANCH \
-            fragaria/karmen-backend:$TRAVIS_BRANCH-amd64 \
-            fragaria/karmen-backend:$TRAVIS_BRANCH-armhf
 
-docker manifest annotate fragaria/karmen-backend:$TRAVIS_BRANCH fragaria/karmen-backend:$TRAVIS_BRANCH-armhf --arch arm
-docker manifest annotate fragaria/karmen-backend:$TRAVIS_BRANCH fragaria/karmen-backend:$TRAVIS_BRANCH-amd64 --arch amd64
+# for bot architecture with major release
+if [[ "$TRAVIS_TAG" =~ ^v[0-9.]*$ ]]; then
+  docker manifest create fragaria/karmen-backend:$TRAVIS_BRANCH \
+              fragaria/karmen-backend:$TRAVIS_BRANCH-amd64 \
+              fragaria/karmen-backend:$TRAVIS_BRANCH-armhf
 
-docker manifest push fragaria/karmen-backend:$TRAVIS_BRANCH
+  docker manifest annotate fragaria/karmen-backend:$TRAVIS_BRANCH fragaria/karmen-backend:$TRAVIS_BRANCH-armhf --arch arm
+  docker manifest annotate fragaria/karmen-backend:$TRAVIS_BRANCH fragaria/karmen-backend:$TRAVIS_BRANCH-amd64 --arch amd64
 
+  docker manifest push fragaria/karmen-backend:$TRAVIS_BRANCH
+
+# otherwise just amd64
+else
+  docker manifest create fragaria/karmen-backend:$TRAVIS_BRANCH \
+              fragaria/karmen-backend:$TRAVIS_BRANCH-amd64 \
+
+  docker manifest annotate fragaria/karmen-backend:$TRAVIS_BRANCH fragaria/karmen-backend:$TRAVIS_BRANCH-amd64 --arch amd64
+
+  docker manifest push fragaria/karmen-backend:$TRAVIS_BRANCH
+
+fi
 # latest only if not rc
 if [[ "$TRAVIS_TAG" =~ ^v[0-9.]*$ ]]; then
   docker pull fragaria/karmen-backend:$TRAVIS_BRANCH
