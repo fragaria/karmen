a9777587a9cfa1a0629e6129a9aa536107818640
Robin Gottfried <robin.gottfried@fragaria.cz>
2020-06-17 08:31:27 +0200

    removed suppport for arm images

--- .travis/build-images-backend.sh
+++ .travis/build-images-backend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -e
+set -eo errtrace
+set -o pipefail
 
 # kudos https://dev.to/zeerorg/build-multi-arch-docker-images-on-travis-5428
 
@@ -18,24 +19,12 @@ buildctl build --frontend dockerfile.v0 \
             --opt platform=linux/amd64 \
             --opt filename=./Dockerfile
 
-
-# Build for armhf and push
-buildctl build --frontend dockerfile.v0 \
-            --local dockerfile=. \
-            --local context=. \
-            --output type=image,name=docker.io/fragaria/karmen-backend:$TRAVIS_BRANCH-armhf,push=true \
-            --opt platform=linux/armhf \
-            --opt filename=./Dockerfile
-
-
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 # Create manifest list and push that
 docker manifest create fragaria/karmen-backend:$TRAVIS_BRANCH \
-            fragaria/karmen-backend:$TRAVIS_BRANCH-amd64 \
-            fragaria/karmen-backend:$TRAVIS_BRANCH-armhf
+            fragaria/karmen-backend:$TRAVIS_BRANCH-amd64
 
-docker manifest annotate fragaria/karmen-backend:$TRAVIS_BRANCH fragaria/karmen-backend:$TRAVIS_BRANCH-armhf --arch arm
 docker manifest annotate fragaria/karmen-backend:$TRAVIS_BRANCH fragaria/karmen-backend:$TRAVIS_BRANCH-amd64 --arch amd64
 
 docker manifest push fragaria/karmen-backend:$TRAVIS_BRANCH
@@ -44,18 +33,13 @@ docker manifest push fragaria/karmen-backend:$TRAVIS_BRANCH
 if [[ "$TRAVIS_TAG" =~ ^v[0-9.]*$ ]]; then
   docker pull fragaria/karmen-backend:$TRAVIS_BRANCH
   docker pull fragaria/karmen-backend:$TRAVIS_BRANCH-amd64
-  docker pull fragaria/karmen-backend:$TRAVIS_BRANCH-armhf
   docker tag fragaria/karmen-backend:$TRAVIS_BRANCH fragaria/karmen-backend:latest
   docker tag fragaria/karmen-backend:$TRAVIS_BRANCH-amd64 fragaria/karmen-backend:latest-amd64
-  docker tag fragaria/karmen-backend:$TRAVIS_BRANCH-armhf fragaria/karmen-backend:latest-armhf
   docker push fragaria/karmen-backend:latest
-  docker push fragaria/karmen-backend:latest-armhf
   docker push fragaria/karmen-backend:latest-amd64
 
   docker manifest create fragaria/karmen-backend:latest \
-              fragaria/karmen-backend:latest-amd64 \
-              fragaria/karmen-backend:latest-armhf
-  docker manifest annotate fragaria/karmen-backend:latest fragaria/karmen-backend:latest-armhf --arch arm
+              fragaria/karmen-backend:latest-amd64
   docker manifest annotate fragaria/karmen-backend:latest fragaria/karmen-backend:latest-amd64 --arch amd64
 
   docker manifest push fragaria/karmen-backend:latest
@@ -69,19 +53,9 @@ curl -i -X DELETE \
   -H "Authorization: JWT ${DOCKER_TOKEN}" \
   "https://hub.docker.com/v2/repositories/fragaria/karmen-backend/tags/${TRAVIS_BRANCH}-amd64/"
 
-curl -i -X DELETE \
-  -H "Accept: application/json" \
-  -H "Authorization: JWT ${DOCKER_TOKEN}" \
-  "https://hub.docker.com/v2/repositories/fragaria/karmen-backend/tags/${TRAVIS_BRANCH}-armhf/"
-
 if [[ "$TRAVIS_TAG" =~ ^v[0-9.]*$ ]]; then
   curl -i -X DELETE \
     -H "Accept: application/json" \
     -H "Authorization: JWT ${DOCKER_TOKEN}" \
     "https://hub.docker.com/v2/repositories/fragaria/karmen-backend/tags/latest-amd64/"
-
-  curl -i -X DELETE \
-    -H "Accept: application/json" \
-    -H "Authorization: JWT ${DOCKER_TOKEN}" \
-    "https://hub.docker.com/v2/repositories/fragaria/karmen-backend/tags/latest-armhf/"
 fi
