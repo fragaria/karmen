f559a310776c04451fba0506a356039ea6c8d02c
Robin Gottfried <robin.gottfried@fragaria.cz>
2020-07-17 12:32:54 +0200

    version raised to 0.13.0-rc12.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-__version__ = "0.13.0-rc12"
+__version__ = "0.13.0-rc12.2"
 
 # -- Project information -----------------------------------------------------
 
