fe9e3007f6dfb1965df1dcd4bf88a8bf4313d0af
Adam Ježek <mail@ajezek.cz>
2020-08-24 13:36:57 +0200

    fixed org list

--- src/reducers/organizations.js
+++ src/reducers/organizations.js
@@ -19,12 +19,8 @@ export default (
 ) => {
   switch (action.type) {
     case "ORGANIZATIONS_LOAD_SUCCEEDED":
-      let o = [];
-        action.payload.data.forEach(e => {
-          o.push({"uuid":e.id, "name": e.name, "role":e.role});
-        });
       return Object.assign({}, state, {
-        list: getSortedOrganizations(o),
+        list: getSortedOrganizations(action.payload.data),
         listLoaded: true,
       });
     case "ORGANIZATIONS_EDIT_SUCCEEDED":
