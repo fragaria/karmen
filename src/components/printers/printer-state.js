fd70f87699e420e3e6f8d332894f3be076cb74b2
Adam Ježek <mail@ajezek.cz>
2020-11-08 22:39:50 +0100

    status labels

--- src/components/printers/printer-state.js
+++ src/components/printers/printer-state.js
@@ -1,65 +1,149 @@
 import React from "react";
+import ReactTooltip from 'react-tooltip';
 
-export const PrinterState = ({ printer }) => {
-  // let printerStatusClass = "";
-  // if (!printer.client.octoprint.version) {
-  //   if (["Operational", "Online"].indexOf(printer.status.state) > -1) {
-  //     printerStatusClass = "text-warning";
-  //   } else if (
-  //     ["Offline", "Closed"].indexOf(printer.status.state) > -1 ||
-  //     printer.status.state.match(/^Printer is not/)
-  //   ) {
-  //     printerStatusClass = "text-normal";
-  //   }
-  // }
-  if (!printer || !printer.client || !printer.client.octoprint) {
-    return <>Status unknown</>
-  }
-  if (printer.client.octoprint.error) {
-    return <>{printer.client.octoprint.error.detail}</>
-  }
-  if (!printer.client.octoprint.printer.state) {
-    return <>Printer disconnected</>
-  }
-  let statuses = []
-  if (printer.client.octoprint.printer.state) {
-    let printerStates = printer.client.octoprint.printer.state;
-    statuses = Object.keys(printerStates).filter((s) => printerStates[s])
-  }
+
+function buildLabels(labels) {
   return (
-    <>Printer is {statuses.join(', ')}</>
-  );
-  // return (
-  //   <>
-  //     {printer.client && printer.client.access_level === "protected" ? (
-  //       <strong className={`text-secondary`}>Authorization required</strong>
-  //     ) : (
-  //       <>
-  //         <span>is </span>
-  //         <strong
-  //           className={
-  //             printer.client && printer.client.connected
-  //               ? "text-success"
-  //               : "text-secondary"
-  //           }
-  //         >
-  //           {printer.client && printer.client.connected
-  //             ? "connected"
-  //             : "disconnected"}
-  //         </strong>
-  //         <span> and </span>
-  //         <strong className={printerStatusClass}>
-  //           {printer.status && printer.status.state
-  //             ? printer.status.state
-  //             : "Unknown"}
-  //         </strong>
-  //         {printer.client && printer.client.access_level === "read_only" && (
-  //           <span className={`tag`}>{"Read only"}</span>
-  //         )}
-  //       </>
-  //     )}
-  //   </>
-  // );
+    <>
+      <ReactTooltip/>
+      <span className="list-item-subtitle">
+     {labels.map(function (label, index) {
+       return <span className={"printer-status-label " + label.color} data-tip={label.detail}> {label.status}</span>
+
+     })}
+      </span>
+    </>
+  )
+}
+
+export const PrinterState = ({printer}) => {
+
+  console.log(printer)
+
+  if (!printer.client || !printer.client.octoprint) {
+    return buildLabels([{color: "gray", status: "Offline"}])
+  }
+
+  let labels = []
+
+
+  if (printer.client.octoprint && printer.client.octoprint.error) {
+    let err = printer.client.octoprint.error;
+    if (err.code === "unknown-response") {
+      // if WS API returns 502 or 504, backend considers it as unknown response, but it means pill is offline
+      return buildLabels([{
+        color: "gray",
+        status: "Offline",
+        detail: "It looks like your device is not connected to the internet."
+      }])
+    }
+    if (err.code === "moved-to-background") {
+      // if WS API returns 502 or 504, backend considers it as unknown response, but it means pill is offline
+      return buildLabels([{color: "orange", status: "Connecting", detail: "We are trying to connect to your device."}])
+    }
+
+    // Offline errors should be handled, now the rest should come from online devices.
+    labels.push({color: "green", status: "Online"});
+
+    if (err.code === "permission-denied") {
+      return buildLabels(labels.concat({
+        color: "red",
+        status: "Authorization error",
+        detail: "Looks like this printer is protected and you did not entered the correct API key"
+      }))
+    }
+    labels.push({color: "red", status: "Error", detail: err.detail})
+  } else {
+    labels.push({color: "green", status: "Online", detail: "We are connected."});
+  }
+
+  if (printer.client.pill.error) {
+    let code = printer.client.pill.error.code;
+    if (code === "unknown-response") { //pill is offline
+      // again, API returns 502, which means pill is offline
+      // but this could be older, cached response, since pill api is set to longer caching
+      // return buildLabels([{color: "gray", status: "Offline"}])
+    } else if (code === "moved-to-background") {
+      // this means getting pill info took to long, no big deal, it's not essential and we could wait for it
+      // return buildLabels([{color: "orange", status: "Connecting"}])
+    } else {
+      // this means we have some unexpected error with pill
+      labels.push({color: "red", status: "Pill error", detail: printer.client.pill.error.detail})
+    }
+  }
+
+
+  if (!printer.client.octoprint || !printer.client.octoprint.printer || Array.isArray(printer.client.octoprint.printer)) {
+    console.log(printer.client.octoprint.printer)
+    if (printer.client.octoprint.printer[0].error) {
+      let err = printer.client.octoprint.printer[0].error;
+      if (err.code === "printer-not-operational") {
+        return buildLabels(labels.concat({
+          color: "orange",
+          status: "Printer disconnected",
+          detail: "Printer is not connected to Pill. Make sure it's powered on and click connect."
+        }));
+      }
+    }
+    return buildLabels(labels.concat({color: "red", status: "Error"}));
+  }
+
+  if (printer.client.octoprint.printer.state.flags) {
+
+    //https://docs.octoprint.org/en/master/api/datamodel.html#printer-state
+    let printerStates = printer.client.octoprint.printer.state.flags;
+
+    if (printerStates.error) {
+      labels.push({color: "red", status: "Error", detail:printer.client.octoprint.printer.state.text})
+    }
+    if (printerStates.cancelling) {
+      labels.push({color: "red", status: "Cancelling"})
+    }
+    if (printerStates.finishing) {
+      labels.push({color: "orange", status: "Finishing"})
+    }
+    if (printerStates.pausing) {
+      labels.push({color: "red", status: "Pausing"})
+    }
+    if (printerStates.paused) {
+      labels.push({color: "red", status: "Paused"})
+    }
+    if (printerStates.resuming) {
+      labels.push({color: "red", status: "Resuming"})
+    }
+    if (printerStates.printing) {
+      labels.push({color: "orange", status: "Printing"})
+    }
+    if (printerStates.closedOrError) {
+      labels.push({color: "orange", status: "Disconnected"})
+    }
+
+    if (printerStates.operational && printerStates.ready) {
+      labels.push({color: "green", status: "Ready to print"})
+    }
+    if (printerStates.operational && !printerStates.ready) {
+      labels.push({color: "orange", status: "Working"})
+    }
+
+  }
+  return buildLabels(labels);
+
+  /*
+  cancelling: false
+  closedOrError: false
+  error: false
+  finishing: false
+  operational: true
+  paused: false
+  pausing: false
+  printing: true
+  ready: false
+  resuming: false
+  sdReady: false
+
+   */
+
+
 };
 
 export default PrinterState;
