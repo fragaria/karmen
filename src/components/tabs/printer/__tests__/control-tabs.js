8f4d77054a29637d8e59eff02cf8457c622501e2
Adam Ježek <jezek.adamek@gmail.com>
2020-11-19 10:26:20 +0100

    Fix travis config (#496)
    * changed path
    
    * editorconfig
    
    * fixed tests
    
    * no need to run build
    
    * changed tests condition

--- src/components/tabs/printer/__tests__/control-tabs.js
+++ src/components/tabs/printer/__tests__/control-tabs.js
@@ -4,76 +4,122 @@ import { render } from "@testing-library/react";
 
 require("jest-fetch-mock").enableMocks();
 
-const createPrinter = (accessLevel, connected, state) => {
+const createPrinter = (unlocked, connected, printing) => {
+  var printer, octoprint;
+
+  if (!unlocked) {
+    //no access
+    octoprint = {
+      error: {
+        detail:
+          "The device indicates that we do not have access to the resource.",
+        code: "permission-denied",
+      },
+    };
+  } else {
+    if (!connected) {
+      //we have access but printer is not connected
+      printer = {
+        error: {
+          detail: "Printer is not operational",
+          code: "printer-not-operational",
+        },
+      };
+    } else {
+      printer = {
+        state: {
+          flags: {
+            cancelling: false,
+            closedOrError: false,
+            error: false,
+            finishing: false,
+            operational: true,
+            paused: false,
+            pausing: false,
+            printing: printing,
+            ready: false,
+            resuming: false,
+            sdReady: false,
+          },
+          text: "Printing",
+        },
+        temperature: {
+          bed: {
+            actual: 60.1,
+            offset: 0,
+            target: 60.0,
+          },
+          tool0: {
+            actual: 214.3,
+            offset: 0,
+            target: 215.0,
+          },
+        },
+        currentJob: {
+          completion: 23.76632945532669,
+          printTime: 7369,
+          printTimeLeft: 23637,
+          name: "Splash_0.2mm_PLA_MK3S_8h54m.gcode",
+        },
+      };
+    }
+
+    octoprint = {
+      version: { api: "0.1", server: "1.3.12", text: "OctoPrint 1.3.12" },
+      plugins: [
+        "action_command_prompt",
+        "awesome_karmen_led",
+        "discovery",
+        "errortracking",
+        "firmwareupdater",
+        "pi_support",
+        "pluginmanager",
+      ],
+      printer: printer,
+      lights: "on",
+    };
+  }
+
   return {
+    id: "rs-mia-fsh",
+    name: "fraga prusa",
+    url: "http://localhost:8000/api/2/printers/rs-mia-fsh/",
+    users: [
+      {
+        url: "http://localhost:8000/api/2/users/xm-0sz-1pt/",
+        id: "xm-0sz-1pt",
+        username: "user",
+        first_name: "",
+        last_name: "",
+      },
+    ],
+    groups: [
+      {
+        url: "http://localhost:8000/api/2/groups/du-np3-k1y/",
+        id: "du-np3-k1y",
+        name: "Group - user is admin",
+      },
+    ],
     client: {
-      access_level: accessLevel,
-      api_key: null,
-      connected: connected,
-      name: "octoprint",
-      pill_info: {
-        karmen_version: "0.2.0",
-        update_available: "0.2.2",
+      octoprint: octoprint,
+      pill: {
+        error: null,
+        version: null,
+        version_string: "",
+        next_version: null,
         update_status: null,
-        version_number: "0.2.0",
-      },
-      plugins: ["awesome_karmen_led"],
-      version: {
-        api: "0.1",
-        server: "0.0.1",
-        text: "octoprint fake",
       },
     },
-    hostname: "karmen_fake_printer1_1.karmen_printers.local",
-    ip: "172.16.236.11",
-    job: {
-      completion: 66.666,
-      name: "fake-file-being-printed.gcode",
-      printTime: 63296,
-      printTimeLeft: 12723,
-    },
-    lights: "off",
-    name: "fake 1",
-    path: "",
-    port: 8080,
-    printer_props: {
-      bed_type: "Powder coated PEI",
-      filament_color: "black",
-      filament_type: "PETG",
-      tool0_diameter: 0.25,
-    },
-    protocol: "http",
-    status: {
-      state: state,
-      temperature: {
-        bed: {
-          actual: 24.7,
-          target: 24.7,
-        },
-        tool0: {
-          actual: 16.2,
-          target: 16.2,
-        },
-      },
-    },
-    token: null,
-    id: "20e91c14-c3e4-4fe9-a066-e69d53324a20",
-    webcam: {
-      flipHorizontal: false,
-      flipVertical: false,
-      rotate90: false,
-      url:
-        "/organizations/b3060e41-e319-4a9b-8ac4-e0936c75f275/printers/20e91c14-c3e4-4fe9-a066-e69d53324a20/webcam-snapshot",
-    },
+    printjobs: [],
+    note: "",
   };
 };
-
 const renderControlTabs = (printer) => {
   return render(
     <ControlsTab
       printer={printer}
       available={undefined}
-      temperatures={printer.status.temperature}
+      temperatures={printer.temperature}
       movePrinthead={() => {}}
       changeFanState={() => {}}
       changeMotorsState={() => {}}
@@ -105,26 +151,26 @@ const expectTextNotInTheDocument = (queryByText) => {
 };
 
 it("Check unlocked connected printer - printing", async () => {
-  const printer = createPrinter("unlocked", true, "Printing");
+  const printer = createPrinter(true, true, true);
   const { getByText } = renderControlTabs(printer);
   expectTextInTheDocument(getByText);
 });
 
 it("Check locked connected printer - printing", async () => {
-  const printer = createPrinter("locked", true, "Printing");
+  const printer = createPrinter(false, true, true);
   const { queryByText } = renderControlTabs(printer);
   expectTextNotInTheDocument(queryByText);
   expect(queryByText("Lights")).not.toBeInTheDocument();
 });
 
 it("Check unlocked disconnected printer - printing", async () => {
-  const printer = createPrinter("unlocked", false, "Printing");
+  const printer = createPrinter(true, false, true);
   const { queryByText } = renderControlTabs(printer);
   expectTextNotInTheDocument(queryByText);
 });
 
 it("Check locked disconnected printer - printing", async () => {
-  const printer = createPrinter("locked", false, "Printing");
+  const printer = createPrinter(false, false, true);
   const { queryByText } = renderControlTabs(printer);
   expectTextNotInTheDocument(queryByText);
   expect(queryByText("Lights")).not.toBeInTheDocument();
@@ -132,8 +178,8 @@ it("Check locked disconnected printer - printing", async () => {
 
 it("Check locked disconnected printer - Printer is not connected to Octoprint", async () => {
   const printer = createPrinter(
-    "unlocked",
     true,
+    false,
     "Printer is not connected to Octoprint"
   );
   const { queryByText } = renderControlTabs(printer);
