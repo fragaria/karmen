3f413ca5c7e6085b82a337f3023b7dc7ed08ffa5
Adam Ježek <mail@ajezek.cz>
2020-12-08 16:10:18 +0100

    another run

--- .travis/create-test-env.sh
+++ .travis/create-test-env.sh
@@ -12,32 +12,29 @@ WORKDIR=$(pwd)
 
 mkdir -p test-tmp
 rm -rf test-tmp/*
+git clone https://${BITBUCKET_USERNAME}:${BITBUCKET_ACCESS_PASSWORD}@bitbucket.org/fragariacz/karmen-backend2.git test-tmp/backend
+
+
+#eval "$(ssh-agent -s)"
+## So travis won't let add ssh keys with public repos
+## So we just take that multiline key and shove it into env var as self-evaluating single-line echo
+#echo ${BITBUCKET_SSH_KEY} > id_rsa_travis
+#
+#cat id_rsa_travis
+#wc -l id_rsa_travis
+## of course there is the 'authenticity can't be established' stuff
+##echo -e "Host bitbucket.org\n\tStrictHostKeyChecking no" > ~/.ssh/config
+#ssh-keyscan -H bitbucket.org >>  ~/.ssh/known_hosts
+#
+#echo "test"
+## gotta make it stop bitching about permissions
+#chmod 600 id_rsa_travis
+#
+#ssh-add id_rsa_travis
+#echo "a"
+#ssh-agent bash -c 'ssh-add id_rsa_travis; git clone git@bitbucket.org:fragariacz/karmen-backend2.git'
 #git clone git@bitbucket.org:fragariacz/karmen-backend2.git test-tmp/backend
-#git -c "http.extraHeader=Authorization: Basic YWRhbWplemVrOTg6dWVMcWZHRGhXUDRhTFVlc1hiUVc=" clone https://adamjezek98@bitbucket.org/fragariacz/karmen-backend2.git
-#git clone https://${BITBUCKET_USERNAME}:${BITBUCKET_ACCESS_PASSWORD}@bitbucket.org/fragariacz/karmen-backend2.git test-tmp/backend
-#git clone https://adamjezek98:tWDhdmx6eY3GdmSFsu9b@bitbucket.org/fragariacz/karmen-backend2.git test-tmp/backend
-
-
-eval "$(ssh-agent -s)"
-# So travis won't let add ssh keys with public repos
-# So we just take that multiline key and shove it into env var as self-evaluating single-line echo
-echo ${BITBUCKET_SSH_KEY} > id_rsa_travis
-
-cat id_rsa_travis
-wc -l id_rsa_travis
-# of course there is the 'authenticity can't be established' stuff
-#echo -e "Host bitbucket.org\n\tStrictHostKeyChecking no" > ~/.ssh/config
-ssh-keyscan -H bitbucket.org >>  ~/.ssh/known_hosts
-
-echo "test"
-# gotta make it stop bitching about permissions
-chmod 600 id_rsa_travis
-
-ssh-add id_rsa_travis
-echo "a"
-ssh-agent bash -c 'ssh-add id_rsa_travis; git clone git@bitbucket.org:fragariacz/karmen-backend2.git'
-git clone git@bitbucket.org:fragariacz/karmen-backend2.git test-tmp/backend
-
+#
 
 
 echo ""
