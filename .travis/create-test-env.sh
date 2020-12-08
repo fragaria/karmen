414dc6c2b871c511e71bff5fe4cd585b6e0671b4
Adam Ježek <mail@ajezek.cz>
2020-12-08 14:49:50 +0100

    another run

--- .travis/create-test-env.sh
+++ .travis/create-test-env.sh
@@ -14,8 +14,13 @@ mkdir -p test-tmp
 rm -rf test-tmp/*
 #git clone git@bitbucket.org:fragariacz/karmen-backend2.git test-tmp/backend
 #git -c "http.extraHeader=Authorization: Basic YWRhbWplemVrOTg6dWVMcWZHRGhXUDRhTFVlc1hiUVc=" clone https://adamjezek98@bitbucket.org/fragariacz/karmen-backend2.git
-git clone https://${BITBUCKET_USERNAME}:${BITBUCKET_ACCESS_PASSWORD}@bitbucket.org/fragariacz/karmen-backend2.git test-tmp/backend
+#git clone https://${BITBUCKET_USERNAME}:${BITBUCKET_ACCESS_PASSWORD}@bitbucket.org/fragariacz/karmen-backend2.git test-tmp/backend
 #git clone https://adamjezek98:tWDhdmx6eY3GdmSFsu9b@bitbucket.org/fragariacz/karmen-backend2.git test-tmp/backend
+
+echo ${BITBUCKET_SSH_KEY} > is_id_rsa_travis
+
+ssh-agent bash -c 'ssh-add id_rsa_travis; git clone git@bitbucket.org:fragariacz/karmen-backend2.git'
+
 
 echo ""
 
